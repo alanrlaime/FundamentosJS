e7acd14ec5edffdc819d8efee8eab2b464b0ab7b
alanrlaime <alanrodrigolaime@gmail.com>
2024-09-06 22:32:46 -0400

    fundamentos de js

---
+++ main.js
@@ -0,0 +1,16 @@
+
+//Eventos al hacer click en un elemento
+document.querySelector('.aqui').addEventListener("click", ()=>{
+    alert("gatos");
+});
+
+
+// bucle for para realizar procesos con cada elemento del Array
+nombreArray = [0,2,4,6]
+nombreArray.forEach(variable_local => {
+    console.log(variable_local);
+});
+
+var n1 = 12;
+var n2 = 34
+console.error("Te han jaquiado prro :v")
